c3731117e365b61598d6e9de837b360079648ed0
danilodev7 <danilodev7@gmail.com>
2024-05-08 17:59:46 -0300

    :ok_hand: Alterações de revisão de código

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -5,6 +5,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5173'
+  });
+
+  //app.enableCors(); // Habilita o CORS para todas as origens
+  
 
   //CONFIGURAÇÃO DO SWAGGER
   const config = new DocumentBuilder()
